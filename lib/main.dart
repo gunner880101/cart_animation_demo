cef6658185feb01e2aa057263ed1c67983333fa4
gunner <gunner880101@hotmail.com>
2022-10-29 22:41:53 +0800

    Tested add_to_cart_animation package in progress

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+// ignore_for_file: library_private_types_in_public_api, non_constant_identifier_names
+
+import 'package:add_to_cart_animation/add_to_cart_animation.dart';
+import 'package:add_to_cart_animation/add_to_cart_icon.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,103 +17,102 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(
+        title: 'Add to cart',
+      ),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  // We can detech the location of the card by this  GlobalKey<CartIconKey>
+  GlobalKey<CartIconKey> gkCart = GlobalKey<CartIconKey>();
+  late Function(GlobalKey) runAddToCardAnimation;
+  var _cartQuantityItems = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return AddToCartAnimation(
+      // To send the library the location of the Cart icon
+      gkCart: gkCart,
+      rotation: true,
+      dragToCardCurve: Curves.easeIn,
+      dragToCardDuration: const Duration(milliseconds: 1000),
+      previewCurve: Curves.linearToEaseOut,
+      previewDuration: const Duration(milliseconds: 500),
+      previewHeight: 30,
+      previewWidth: 30,
+      opacity: 0.85,
+      initiaJump: false,
+      receiveCreateAddToCardAnimationMethod: (addToCardAnimationMethod) {
+        // You can run the animation by addToCardAnimationMethod, just pass trough the the global key of  the image as parameter
+        runAddToCardAnimation = addToCardAnimationMethod;
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(widget.title),
+          centerTitle: false,
+          actions: [
+            // Improvement/Suggestion 4.4 -> Adding 'clear-cart-button'
+            IconButton(
+              icon: const Icon(Icons.cleaning_services),
+              onPressed: () {
+                _cartQuantityItems = 0;
+                gkCart.currentState!.runClearCartAnimation();
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            const SizedBox(width: 16),
+            AddToCartIcon(
+              key: gkCart,
+              icon: const Icon(Icons.shopping_cart),
+              colorBadge: Colors.red,
             ),
+            const SizedBox(
+              width: 16,
+            )
+          ],
+        ),
+        body: ListView(
+          children: [
+            AppListItem(onClick: listClick, index: 1),
+            AppListItem(onClick: listClick, index: 2),
+            AppListItem(onClick: listClick, index: 3),
+            AppListItem(onClick: listClick, index: 4),
+            AppListItem(onClick: listClick, index: 5),
+            AppListItem(onClick: listClick, index: 6),
+            AppListItem(onClick: listClick, index: 7),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+
+  // Improvement/Suggestion 4.4 -> Running AddTOCartAnimation BEFORE runCArtAnimation
+  void listClick(GlobalKey gkImageContainer) async {
+    await runAddToCardAnimation(gkImageContainer);
+    await gkCart.currentState!
+        .runCartAnimation((++_cartQuantityItems).toString());
+  }
+
+  Widget AppListItem({required Function onClick, required int index}) {
+    return ListTile(
+      leading: Image.asset(
+        'assets/images/apple.jpeg',
+        height: 200,
+        fit: BoxFit.fitHeight,
+      ),
+      title: Text("Animated Apple $index"),
     );
   }
 }
